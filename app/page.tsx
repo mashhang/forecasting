b912dfc1d53a75e62b3cc53eadbc2035f8a3efa3
edrel <edrell43@gmail.com>
2025-10-07 15:41:03 +0800

    improve login design

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/navigation";
 import { useAuth } from "./context/AuthContext";
 import Image from "next/image";
 import { toast } from "sonner";
-import Logo from "../../public/logo.png";
+import Logo2 from "../public/logo2.png";
 import getApiUrl from "@/lib/getApiUrl";
 import { CgSpinner } from "react-icons/cg";
 
@@ -64,9 +64,11 @@ export default function Login() {
   };
 
   return (
-    <div className="h-screen w-screen flex justify-center items-center">
-      <div className="flex flex-col w-[400px] p-6 border border-black rounded-xl">
-        <h1 className="text-lg font-semibold">Login</h1>
+    <div className="flex flex-col items-center justify-center h-screen w-screen">
+      <Image src={Logo2} alt="logo" height={300} width={300} className="mb-4"/>
+    <div className="flex justify-center items-center">
+      <div className="flex flex-col w-[400px] p-6  shadow-lg rounded-xl">
+        <h1 className="text-xl font-semibold text-center">LOGIN</h1>
         <form
           className="mt-4 space-y-2"
           noValidate
@@ -74,28 +76,28 @@ export default function Login() {
             e.preventDefault();
             handleLogin();
           }}
-        >
-          <p>Email</p>
+          >
+          <p className="mb-1 text-gray-800">Email</p>
           <input
             type="text"
-            className="w-full rounded-xl border px-3 py-2"
-            placeholder="name@loa.edu.ph"
+            className="w-full rounded-xl border border-gray-300 px-3 py-2 shadow-sm hover:shadow-md transition-all"
+            placeholder="name@email.com"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-          />
-          <p>Password</p>
+            />
+          <p className="mb-1 text-gray-800">Password</p>
           <input
             type="password"
-            className="w-full rounded-xl border px-3 py-2"
+            className="w-full rounded-xl border border-gray-300 px-3 py-2 shadow-sm hover:shadow-md transition-all"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             placeholder="••••••••"
-          />
+            />
           <button
             type="submit"
             disabled={isLoggingIn}
-            className="w-full bg-[var(--brand-green)] text-white hover:bg-[#228057d3] rounded-xl p-2 mt-4 flex justify-center items-center"
-          >
+            className="w-full bg-[var(--brand-green)] text-white hover:bg-[#228057d3] rounded-xl p-2 mt-4 flex justify-center items-center shadow-sm hover:shadow-md transition-all"
+            >
             {isLoggingIn ? (
               <>
                 <CgSpinner className="animate-spin mr-2" />
@@ -108,5 +110,6 @@ export default function Login() {
         </form>
       </div>
     </div>
+            </div>
   );
 }
